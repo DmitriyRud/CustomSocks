78bf7aa81f8b98815229608159517cb43a337dba
jafarMagomedov <jafarmafomedov0594@mail.ru>
2022-02-23 15:49:49 +0300

    Start

---
+++ app.js
@@ -0,0 +1,46 @@
+const exspress = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv').config();
+
+const PORT = 3000;
+
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const indexRouter = require('./router/indexRouter');
+const usersRouter = require('./router/usersRouter');
+
+const app = exspress();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(exspress.json());
+app.use(exspress.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(exspress.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: 'snovaisnova',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+    name: 'login',
+    store: new FileStore(),
+  }),
+);
+
+// миддлвара для защиты ручек. Значение берется из сессии
+app.use((req, res, next) => {
+  res.locals.userId = req.session?.userId;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log('Сервер Работает');
+});
