ca714cc0263645889df6eede73a98739ea055c2e
Dmitriy Rudakov <cska2004@gmail.com>
2022-02-24 17:30:39 +0300

    Работает добавление в избранное

--- db/seeders/20220223161559-Color.js
+++ db/seeders/20220223161559-Color.js
@@ -3,11 +3,27 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
      await queryInterface.bulkInsert('Colors', [{
-      color: '#fff',
+      color: '#FFFFFF',
       createdAt: new Date(),
       updatedAt: new Date(),
      }, {
-      color: '#000',
+      color: '#000000',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     }, {
+      color: '#0000FF',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     }, {
+      color: '#008000',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     }, {
+      color: '#FFD700',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     }, {
+      color: '#FCA099',
       createdAt: new Date(),
       updatedAt: new Date(),
      }], {});
@@ -19,3 +35,4 @@ module.exports = {
 
   }
 };
+
--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {Design, Color, Image, Pattern} = require('../db/models');
+const {Design, Color, Image, Pattern, Favorite} = require('../db/models');
 
 const router = express.Router();
 
@@ -18,7 +18,8 @@ router.post('/favorites', async (req, res)=> {
   const colorId = await Color.findOne({where: {color}});
   const imageUrl = await Image.findOne({where: {url: image}});
   const patternUrl = await Pattern.findOne({where:{url: pattern}});
-  console.log({colorId, imageUrl, patternUrl});
-  //const newDesign = await Design.create({color_id: colorId.id, image_id: imageUrl.id, pattern_id: patternUrl.id});
+  //console.log({colorId, imageUrl, patternUrl});
+  const newDesign = await Design.create({color_id: colorId.id, image_id: imageUrl.id, pattern_id: patternUrl.id});
+  const newFavorite = await Favorite.create({user_id: req.session.userId, design_id: newDesign.id});
   res.sendStatus(202);
 });
